b27783a0dbfe7bcafabce7bfe3eb0fc8f6e5097a
Petr Onderka <petro@postsharp.net>
2020-11-18 18:04:58 +0100

    Initial commit

---
+++ Caravela.Open.AutoCancellationToken/AutoCancellationTokenAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using Caravela.Framework.Aspects;
+
+namespace Caravela.Open.AutoCancellationToken
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface)]
+    public class AutoCancellationTokenAttribute : Attribute, IAspect { }
+}
