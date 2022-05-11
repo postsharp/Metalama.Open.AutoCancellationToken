1b63b6de2550703848475525bae2c18476f7aa29
Jan Hlavac <jan@postsharp.net>
2022-05-11 16:49:08 +0200

    Updated to the newest Engineering.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -192,6 +192,10 @@ object PublicDeployment : BuildType({
 
     dependencies {
 
+        snapshot(AbsoluteId("Metalama_Metalama_PublicDeployment")) {
+                     onDependencyFailure = FailureAction.FAIL_TO_START
+                }
+
         dependency(PublicBuild) {
             snapshot {
                 onDependencyFailure = FailureAction.FAIL_TO_START
