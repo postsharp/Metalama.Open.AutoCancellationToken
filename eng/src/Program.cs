7d9f73759e52473a02978af8612ab6e802a82d44
Jan Hlavac <jan@postsharp.net>
2022-05-24 11:54:54 +0200

    #30382 Using default publishers, Engineering update.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -2,17 +2,23 @@
 // This project is not open source. Please see the LICENSE.md file in the repository root for details.
 
 using PostSharp.Engineering.BuildTools;
+using PostSharp.Engineering.BuildTools.Build;
 using PostSharp.Engineering.BuildTools.Build.Model;
+using PostSharp.Engineering.BuildTools.Build.Publishers;
 using PostSharp.Engineering.BuildTools.Build.Solutions;
 using PostSharp.Engineering.BuildTools.Dependencies.Model;
 using Spectre.Console.Cli;
+using System.Linq;
 
-var product = new Product(Dependencies.MetalamaOpenAutoCancellationToken)
+var product = new Product( Dependencies.MetalamaOpenAutoCancellationToken )
 {
-    Solutions = new[] { new DotNetSolution( "Metalama.Open.AutoCancellationToken.sln" ) { CanFormatCode = true } },
+    Solutions = new Solution[] { new DotNetSolution( "Metalama.Open.AutoCancellationToken.sln" ) { CanFormatCode = true } },
     PublicArtifacts = Pattern.Create( "Metalama.Open.AutoCancellationToken.$(PackageVersion).nupkg" ),
     Dependencies = new[] { Dependencies.PostSharpEngineering, Dependencies.Metalama },
-    RequiresBranchMerging = true
+    Configurations = Product.DefaultConfigurations
+        .WithValue( BuildConfiguration.Public, new BuildConfigurationInfo(
+            MSBuildName: "Release",
+            PublicPublishers: Product.DefaultPublicPublishers.Add( new MergePublisher() ).ToArray() ) )
 };
 
 var commandApp = new CommandApp();
